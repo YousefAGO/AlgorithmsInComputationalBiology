2c9893de2d65e94f4c4000eadde59780610e943e
YousefAGO <97043151+YousefAGO@users.noreply.github.com>
2025-01-26 13:04:04 +0200

    Update AMP.py

--- AMP.py
+++ AMP.py
@@ -9,6 +9,8 @@ def preprocess_data(data_path, output_path, label):
 
     # Initialize variables
     sequences = []
+    lengths = []
+    length = 0
     current_sequence = ""
 
     # Read and process the FASTA file
@@ -18,16 +20,20 @@ def preprocess_data(data_path, output_path, label):
             if line.startswith(">"):  # If it's a new header, save the previous sequence
                 if current_sequence:
                     sequences.append(current_sequence)  # Save the completed sequence
+                    lengths.append(length)  # Save the length of the sequence
                 current_sequence = ""  # Reset for the new sequence
+                length = 0  # Reset for the new sequence
             else:
                 current_sequence += line  # Concatenate sequence lines
+                length += len(line)  # Update the length of the sequence
 
     # Append the last sequence if it exists
     if current_sequence:
         sequences.append(current_sequence)
+        lengths.append(length)
 
     # Create a DataFrame with Label = 0 (Non-AMPs)
-    df = pd.DataFrame({"sequence": sequences, "label": label})
+    df = pd.DataFrame({"sequence": sequences, "label": label, "length": lengths})
 
     # Save to CSV
     df.to_csv(csv_file, index=False)
