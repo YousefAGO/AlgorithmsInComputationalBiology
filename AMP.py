8e8e116d4076f421c43b4afbe84f5b7d8867a258
YousefAGO <97043151+YousefAGO@users.noreply.github.com>
2025-01-26 12:48:25 +0200

    NON-AMP
    NON-AMP

--- AMP.py
+++ AMP.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-def preprocess_data():
+def preprocess_data(data_path, output_path, label):
     import pandas as pd
 
     # Load FASTA file and remove empty rows & headers
-    fasta_file = "humanAMPs_APD2024.fasta.txt"
+    fasta_file = data_path
     sequences = []
 
     with open(fasta_file, "r") as file:
@@ -14,11 +14,11 @@ def preprocess_data():
                 sequences.append(line)
 
     # Create CSV file
-    df = pd.DataFrame({"sequence": sequences, "label": 1})
-    df.to_csv("human_AMPs_dataset.csv", index=False)
+    df = pd.DataFrame({"sequence": sequences, "label": label})
+    df.to_csv(output_path, index=False)
 
-    print("✅ CSV file saved as 'human_AMPs_dataset.csv'")
+    print(f"✅ CSV file saved as %s" % output_path)
 
 
 if __name__ == '__main__':
-    preprocess_data()
+    preprocess_data("uniprotkb_AND_reviewed_true_AND_model_o_2025_01_26.fasta", "none-AMP.csv", 0)
