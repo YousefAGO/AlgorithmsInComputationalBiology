02fbd2946aac916cff22ed55839cb61c898c4396
YousefAGO <97043151+YousefAGO@users.noreply.github.com>
2025-01-27 09:30:12 +0200

    pre process data

--- preprocess.py
+++ preprocess.py
@@ -2,16 +2,12 @@ import random
 random.seed(485)
 import pandas as pd
 
-MAX_SEQ_NUM = 10000
+MAX_SEQ_NUM = 20000
 NUMBER_OF_SEQ = 154
 
 
-def preprocess_data(data_path, output_path, label):
+def preprocess_data(data_path1, output_path, label, split=0):
     import pandas as pd
-
-    # Define input and output file names
-    fasta_file = data_path  # Change this if needed
-    csv_file = output_path
 
     # Initialize variables
     sequences = []
@@ -20,7 +16,7 @@ def preprocess_data(data_path, output_path, label):
     current_sequence = ""
     count = 0
     # Read and process the FASTA file
-    with open(fasta_file, "r") as file:
+    with open(data_path1, "r") as file:
         for line in file:
             line = line.strip()
             count += 1
@@ -40,10 +36,10 @@ def preprocess_data(data_path, output_path, label):
         sequences.append(current_sequence)
         lengths.append(length)
 
-    print(f"✅ Conversion complete! Saved as: {csv_file}")
+    print(f"✅ Conversion complete! Saved as: {output_path}")
 
     # Function to extract random peptide fragments
-    def extract_random_peptides(protein_seq, num_peptides=3, min_len=5, max_len=200):
+    def extract_random_peptides(protein_seq, num_peptides=4, min_len=5, max_len=60):
         peptides = []
         seq_len = len(protein_seq)
 
@@ -57,31 +53,32 @@ def preprocess_data(data_path, output_path, label):
             peptides.append(protein_seq[start_pos:start_pos + peptide_len])
 
         return peptides
+    if split:
+        # Extract random peptides from each protein sequence
+        random_peptides = []
+        i = 0
+        for protein_seq in sequences:
+            l = lengths[i]
+            seq = extract_random_peptides(protein_seq, num_peptides=int(random.uniform(0.5, 0.9)*(l)) + 1, min_len=7, max_len=120)
+            random_peptides.extend(seq)  # Extract 3 peptides per protein
+            if len(random_peptides) >= MAX_SEQ_NUM:
+                break
+        lengths = []
+        for peptide in random_peptides:
+            lengths.append(len(peptide))
+        # sample NUMBER_OF_SEQ indexes from the list of random peptides
+        index_list = range(len(random_peptides))
+        sampled_indexes = random.sample(index_list, NUMBER_OF_SEQ)
+        random_peptides = [random_peptides[i] for i in sampled_indexes]
+        lengths = [lengths[i] for i in sampled_indexes]
 
-    # Extract random peptides from each protein sequence
-    random_peptides = []
-    i = 0
-    for protein_seq in sequences:
-        l = lengths[i]
-        seq = extract_random_peptides(protein_seq, num_peptides=int(random.uniform(0.7, 0.9)*(l)) + 1)
-        random_peptides.extend(seq)  # Extract 3 peptides per protein
-        if len(random_peptides) >= MAX_SEQ_NUM:
-            break
-    lengths = []
-    for peptide in random_peptides:
-        lengths.append(len(peptide))
-
-    # sample NUMBER_OF_SEQ indexes from the list of random peptides
-    index_list = range(len(random_peptides))
-    sampled_indexes = random.sample(index_list, NUMBER_OF_SEQ)
-    random_peptides = [random_peptides[i] for i in sampled_indexes]
-    lengths = [lengths[i] for i in sampled_indexes]
-
+    else:
+        random_peptides = sequences
     # Create a DataFrame with Label = 0 (Non-AMPs)
-    df = pd.DataFrame({"sequence": random_peptides[:NUMBER_OF_SEQ], "label": [label]*NUMBER_OF_SEQ})
+    df = pd.DataFrame({"sequence": random_peptides, "label": [label]*NUMBER_OF_SEQ})
 
     # Save to CSV
-    df.to_csv(csv_file, index=False)
+    df.to_csv(output_path, index=False)
 
 
 def draw_len_hist():
@@ -113,11 +110,13 @@ def merge_files(path1, path2, output_path):
 
 
 if __name__ == '__main__':
-    # preprocess_data("uniprotkb_AND_reviewed_true_AND_model_o_2025_01_26.fasta", "none-AMP4.csv", 0)
-    # preprocess_data("uniprotkb_AND_reviewed_true_AND_model_o_2025_01_26.fasta", "none-AMP5.csv", 0)
+    # preprocess_data("mouseProtein.fasta", "none-AMP11.csv", 0, 1)
+    # preprocess_data("animalAMPs.fasta.txt", "animalAMPs.csv", 1, 0)
+    # preprocess_data("humanAMPs.fasta.txt", "humanAMPs.csv", 1, 0)
+    preprocess_data("uniprotkb_AND_reviewed_true_AND_model_o_2025_01_26.fasta", "none-AMP3.csv", 0, 1)
 
     # merge the two csv files
-    path1 = "none-AMP4.csv"
+    path1 = "none-AMP3.csv"
     path2 = "human_AMPs_dataset.csv"
     output_path = "merged_dataset.csv"
     # merge_files(path1, path2, output_path)
